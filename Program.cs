26df722d812f7ae9649a2eb30f5653c69ecf482c
hrishikesh kamble <kamblehrishi13@gmail.com>
2022-06-03 13:19:18 +0530

    line length push

--- Program.cs
+++ Program.cs
@@ -5,6 +5,21 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Line Comparison Program");
+
+            Console.WriteLine("enter x1");
+            int x1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter y1");
+            int y1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter x2");
+            int x2=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter y2");
+            int y2=Convert.ToInt32(Console.ReadLine());
+
+            var Line = Math.Sqrt((Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)));
+            Console.WriteLine(Line);
+
+
+
         }
     }
 }
